a5987247c039b9de9dc35c2c0e5fd26412a371d9
Ladislau Ciocan <ladislau.ciocan@turistintransilvania.com>
2025-04-13 13:31:05 +0300

    fix for render

--- backend/main.py
+++ backend/main.py
@@ -31,4 +31,7 @@ def start():
 
 if __name__ == "__main__":
     Thread(target=start, daemon=True).start()
-    app.run(host="0.0.0.0", port=5001)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
